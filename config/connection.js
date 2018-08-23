a71fa5ac30516cfdba54fb93e008cc1800e78e07
Alex-Preissler <AlexJPreissler@Outlook.com>
2018-08-23 09:45:35 -0400

    JAWS_DB setup

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "Alex-Preissler",
-  password: "Paintball426*",
-  database: "burgers_db"
+var connection;
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+ connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "Alex-Preissler",
+    password: "Paintball426*",
+    database: "burgers_db"
 });
 
 connection.connect(function(err) {
